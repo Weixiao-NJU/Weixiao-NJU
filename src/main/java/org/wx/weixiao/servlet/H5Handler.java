4f28a982b98ec75b9181741e694b05e7eb7abfc0
WJerry0227 <wangjiawei0227@163.com>
2017-11-07 20:49:19 +0800

    Update H5 url

--- src/main/java/org/wx/weixiao/servlet/H5Handler.java
+++ src/main/java/org/wx/weixiao/servlet/H5Handler.java
@@ -33,7 +33,7 @@ public class H5Handler extends AbstractHandler {
         try {
             if (parameters.get("wxcode") == null) {
                 session.setAttribute("queryType", parameters.get("queryType"));
-                String finalPage = request.getRequestURL()+"?"+request.getQueryString();
+                String finalPage = request.getRequestURL()+"?queryType="+parameters.get("queryType")+"&type="+parameters.get("trigger");
                 String redirectLink = String.format(WEIXIAO_AUTH, parameters.get(NameUtil
                         .MEDIAID), config.getApiKey(),URLEncoder.encode(finalPage));
                 logger.info("The link is redirecting to "+redirectLink);
